2f96ccb71ea2db2b9a5de92eb252195adfe80513
fernandojr0410 <fernando.junior300@hotmail.com>
2023-08-31 15:48:49 -0300

    Começando a realizar a validação do Email

--- js/index.js
+++ js/index.js
@@ -13,19 +13,21 @@ ${nomeCompleto}
 ${cpf}
 ${celular}
 ${email}`);
+
     document.getElementById("nomeCliente").value = "";
     document.getElementById("cpfCliente").value = "";
     document.getElementById("celularCliente").value = "";
     document.getElementById("emailCliente").value = "";
+
+    document.getElementById("mensagemErroNome").textContent = "";
+    document.getElementById("mensagemCorretoNome").textContent = "";
+    document.getElementById("mensagemErroCPF").textContent = "";
+    document.getElementById("mensagemCorretoCPF").textContent = "";
+    document.getElementById("mensagemErroCelular").textContent = "";
+    document.getElementById("mensagemCorretoCelular").textContent = "";
   }
 }
 
-function todosDigitosIguaisNome(nomeCompleto) {
-  const nomeCliente = nomeCompleto[0];
-  return nomeCompleto.split("").every((digito) => {
-    digito === nomeCliente;
-  });
-}
 function todosDigitosIguaisCPF(cpf) {
   const cpfCliente = cpf[0];
   return cpf.split("").every((digito) => digito === cpfCliente);
@@ -37,20 +39,20 @@ function todosDigitosIguaisCelular(celular) {
 }
 
 function validarNomeCompleto() {
-  let nomeCompleto = document.getElementById("nomeCliente").value;
-  let erroNome = document.getElementById("mensagemErroNome");
-  let nomeCorreto = document.getElementById("mensagemCorretoNome");
+  let nomeCliente = document.getElementById("nomeCliente");
+  let mensagemErroNome = document.getElementById("mensagemErroNome");
+  let mensagemCorretoNome = document.getElementById("mensagemCorretoNome");
+  let nome = nomeCliente.value;
 
-  if (nomeCompleto < 3) {
-    erroNome.textContent =
-      "Nome Inválido, nome precisa ter no minimo 3 caracteres.";
-    erroNome.textContent = "";
-  } else if (todosDigitosIguaisNome(nomeCompleto)) {
-    erroNome.textContent =
-      "Nome Inválido, nome não pode ter todos os caracteres iguais";
-    erroNome.textContent = "";
+  if (/[\d]/.test(nome)) {
+    mensagemErroNome.textContent = "Não é permitido ter números neste campo.";
+    mensagemCorretoNome.textContent = "";
+  } else if (nome.length < 3) {
+    mensagemErroNome.textContent = "Nome muito curto.";
+    mensagemCorretoNome.textContent = "";
   } else {
-    nomeCorreto.textContent = "Nome Válido";
+    mensagemErroNome.textContent = "";
+    mensagemCorretoNome.textContent = "Nome Válido";
   }
 }
 
@@ -79,14 +81,31 @@ function validarTamanhoCelular() {
   let celularCorreto = document.getElementById("mensagemCorretoCelular");
 
   if (celular.length !== 11 && celular.length !== 10) {
-    celular.textContent =
+    erroCelular.textContent =
       "Celular Inválido, celular precisa ter exatamente 10 ou 11 caracteres.";
-    erroCelular.textContent = "";
+    celularCorreto.textContent = "";
   } else if (todosDigitosIguaisCelular(celular)) {
     erroCelular.textContent =
       "Celular Inválido, Celular não pode ter todos os dígitos iguais.";
-    erroCelular.textContent = "";
+    celularCorreto.textContent = "";
   } else {
+    erroCelular.textContent = "";
     celularCorreto.textContent = "Celular Válido";
+  }
+}
+
+function validarEmail() {
+  let email = document.getElementById("emailCliente").value;
+  let erroEmail = document.getElementById("mensagemErroEmail");
+  let emailCorreto = document.getElementById("mensagemCorretoEmail");
+
+  let validarEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!validarEmail.test(email)) {
+    erroEmail.textContent = "Email Inválido";
+    emailCorreto.textContent = "";
+  } else {
+    erroEmail.textContent = "";
+    emailCorreto.textContent = "E-mail Válido";
   }
 }
